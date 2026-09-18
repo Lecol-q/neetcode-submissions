18a221cd1bc657adf012e518805ab454084f9ea2
Collin Le <collin_le@outlook.com>
2026-09-18 18:01:58 -0400

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // if prices[i] > prices[k] return 0 as it is not profitable
+        // if prices[i] < prices[k] / return prices[k] - prices[i]
+        int max = 0;
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+
+        while(right < prices.length){
+            if(prices[left] > prices[right]){
+                left = right;
+                right++;
+            }
+            else{
+                profit = prices[right] - prices[left];
+                if(profit > max){
+                    max = profit;
+                }
+                right++;
+            }
+            
+        }
+        return max;
+    }
+}
